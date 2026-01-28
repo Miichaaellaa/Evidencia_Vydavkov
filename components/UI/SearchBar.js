d0d71ae6b897574c92c3ac3c03b6a3406dc53f3f
Štofanová <stofanovamichaela@gmail.com>
2026-01-28 10:00:22 +0100

    Dizajn Search baru

--- components/UI/SearchBar.js
+++ components/UI/SearchBar.js
@@ -5,13 +5,17 @@ import { GlobalStyles } from "../../constant/styles";
 function SearchBar({ placeholder, value, onChangeText }) {
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder={placeholder}
-        value={value}
-        onChangeText={onChangeText}
-      />
-    <Ionicons name="search" size={20} style={styles.icon} />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder={placeholder}
+          value={value}
+          onChangeText={onChangeText}
+        />
+      </View>
+      <View style={styles.iconContainer}>
+        <Ionicons name="search" size={20} style={styles.icon} />
+      </View>
     </View>
   );
 }
@@ -24,16 +28,40 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     marginVertical: 10,
     height: 35,
+    backgroundColor: GlobalStyles.colors.primary500,
+    borderBottomRightRadius: 70,
+    borderTopRightRadius: 60,
+  },
+
+  inputContainer: {
+    width: "97%",
     backgroundColor: GlobalStyles.colors.primary50,
+    marginLeft: -10,
+    borderRadius: 6,
+    paddingHorizontal: 10,
+    borderBottomRightRadius: 60,
   },
+
   icon: {
-    marginRight: 8,
-    color: GlobalStyles.colors.primary500,
+    color: GlobalStyles.colors.primary50,
   },
+
+  iconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: GlobalStyles.colors.primary500,
+    height: 40,
+    width: 40,
+    marginLeft: -10,
+    borderRadius: 90,
+    marginBottom: 5,
+  },
+
   input: {
     flex: 1,
     fontSize: 16,
     color: GlobalStyles.colors.primary400,
+
   },
 });
 
